ff952a6f822a2b86b06f7187aef1d08cf395304c
Julia Fortt <juliamfortt@gmail.com>
2025-05-19 12:02:20 -0400

    add initial project section template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Profile from './sections/Profile/Profile'
+import Projects from './sections/Projects/Projects'
 
 function App() {
   return <>
     <Profile/>
+    <Projects/>
   </>
 }
 
---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from './ProjectsStyles.module.css';
+import moleMaker from '../../assets/React.svg'
+import trashBanditsRecycled from '../../assets/React.svg'
+import autonomousArduinoRobot from '../../assets/React.svg'
+import PIDLineFollower from '../../assets/React.svg'
+import ProjectCard from "../../common/ProjectCard";
+
+function Projects() {
+    return (
+        <section id="projects" className={styles.container}>
+            <h1 className='sectionTitle'>Projects</h1>
+            <div className={styles.projectsContainer}>
+                <ProjectCard    
+                src={moleMaker} 
+                link="https://github.com/jmfortt"
+                h3="MoleMaker"
+                p="Video Game"
+                />
+                <ProjectCard 
+                src={trashBanditsRecycled} 
+                link="https://github.com/jmfortt"
+                h3="Trash Bandits:Recycled"
+                p="Video Game"
+                />
+                <ProjectCard 
+                src={autonomousArduinoRobot} 
+                link="https://github.com/jmfortt"
+                h3="Autonomous Arduino Robot"
+                p="Robotics Project"
+                />
+                <ProjectCard 
+                src={PIDLineFollower} 
+                link="https://github.com/jmfortt"
+                h3="PID Line Follower"
+                p="Robotics Project"
+                />
+            </div>
+        </section>
+        );
+}
+
+export default Projects;
