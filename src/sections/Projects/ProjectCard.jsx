0abf327cbd65d3ddb792d0622f87b8c772810af7
Julia Fortt <juliamfortt@gmail.com>
2025-06-29 10:36:44 -0500

    adjust project card structure

--- src/sections/Projects/ProjectCard.jsx
+++ src/sections/Projects/ProjectCard.jsx
@@ -5,40 +5,44 @@ import Tag from "../../common/Tag";
 
 function ProjectCard({ h1, h2, p, images, links, tags }) {
   return (
-    <section className={styles.projectCard}>
-      <div className={styles.title}>{h1}</div>
-      <div className={styles.projectCardContent}>
+    <section className={styles.container}>
+      <div className={styles.content}>
         <div className={styles.left}>
-          <p className={styles.text} style={{ fontSize: "24px" }}>
-            {h2}
-          </p>
-          <br />
-          <p className={styles.text}>{p}</p>
-          {links.length > 0 && (
-            <ul>
-              {links.map(({ text, url }, i) => (
-                <li key={i}>
-                  <a
-                    className={styles.link}
-                    href={url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          )}
+          <div className={styles.top}>
+            <div className={styles.title}>{h1}</div>
+            <p className={styles.subtitle}>{h2}</p>
+          </div>
+
+          <div className={styles.middle}>
+            <p className={styles.text}>{p}</p>
+            {links.length > 0 && (
+              <ul>
+                {links.map(({ text, url }, i) => (
+                  <li key={i}>
+                    <a
+                      className={styles.link}
+                      href={url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {text}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          <div className={styles.bottom}>
+            <div className="tags" style={{ marginBottom: "0px" }}>
+              {tags && tags.map((tag, i) => <Tag key={i} tag={tag} />)}
+            </div>
+          </div>
         </div>
 
         <div className={styles.right}>
           <Slideshow images={images} />
         </div>
-      </div>
-
-      <div className="tags">
-        {tags && tags.map((tag, i) => <Tag key={i} tag={tag} />)}
       </div>
     </section>
   );
