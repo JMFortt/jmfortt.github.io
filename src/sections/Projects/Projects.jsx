c25eef87c96ac0718e81bfaaa3c4e7eadac8c4a2
Julia Fortt <juliamfortt@gmail.com>
2025-06-29 11:22:07 -0500

    add to robotics projects description

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -13,8 +13,9 @@ import trashBanditsRecycledImg3 from "../../assets/trashBanditsRecycled/img3.png
 import trashBanditsRecycledImg4 from "../../assets/trashBanditsRecycled/img4.png";
 import trashBanditsRecycledImg5 from "../../assets/trashBanditsRecycled/img5.png";
 
-import autonomousArduinoRobotImg1 from "../../assets/autonomousArduinoRobot/img1.jpg";
-import autonomousArduinoRobotImg2 from "../../assets/autonomousArduinoRobot/img2.jpg";
+import autonomousArduinoRobotImg1 from "../../assets/autonomousArduinoRobot/img1.jpeg";
+import autonomousArduinoRobotImg2 from "../../assets/autonomousArduinoRobot/img2.jpeg";
+import autonomousArduinoRobotImg3 from "../../assets/autonomousArduinoRobot/img3.jpeg";
 
 import PIDLineFollowerImg1 from "../../assets/PIDLineFollower/img1.jpg";
 
@@ -26,7 +27,9 @@ function Projects() {
         <ProjectCard
           h1="MoleMaker"
           h2="An educational video game for 7th-12th graders."
-          p={`Developed as a final project for MIT's CMS.590 Design and Development of Games for Learning course, I designed this game to reinforce highschool-level chemistry topics as part of a team of 4. \n \n My main role on the team was as a programmer. In addition, I wrote the background music track in BeepBox, organized playtests, and took lead in organizing the group's work via project tasklists and sprints.`}
+          p={`Developed as a final project for MIT's CMS.590 Design and Development of Games for Learning course, I designed this game to reinforce highschool-level chemistry topics as part of a team of 4.
+          
+          My main role on the team was as a programmer. In addition, I wrote the background music track in BeepBox, organized playtests, and took lead in organizing the group's work via project tasklists and sprints.`}
           images={[molemakerImg1, molemakerImg2, molemakerImg3, molemakerImg4]}
           links={[
             {
@@ -43,7 +46,11 @@ function Projects() {
         <ProjectCard
           h1="Trash Bandits:Recycled"
           h2="A collaborative 2-player video game."
-          p={`Originally developed as a final project for MIT's CMS.611 Creating Video Games course as part of a team of 6, I continued development solo after the course was completed to create this "recycled" release. \n \n My main role on the team was as a programmer, though I also assisted with project management tasks and was in charge of fixing any technical issues that arose during development. \n \n During solo development, I gave the game a visual overhaul using custom sprites made in Aseprite, cleaned up project structure, and fixed lingering bugs.`}
+          p={`Originally developed as a final project for MIT's CMS.611 Creating Video Games course as part of a team of 6, I continued development solo after the course was completed to create this "recycled" release.
+          
+          My main role on the team was as a programmer, though I also assisted with project management tasks and was in charge of fixing any technical issues that arose during development. 
+          
+          During solo development, I gave the game a visual overhaul using custom sprites made in Aseprite, cleaned up project structure, and fixed lingering bugs.`}
           images={[
             trashBanditsRecycledImg1,
             trashBanditsRecycledImg2,
@@ -70,15 +77,25 @@ function Projects() {
         <ProjectCard
           h1="Autonomous Arduino Robot"
           h2="Robotics project."
-          p={`Built as part of MIT's Design & Manufacturing I: Autonomous Machines course.`}
-          images={[autonomousArduinoRobotImg1, autonomousArduinoRobotImg2]}
+          p={`Built as part of MIT's 16.632B Introduction to Autonomous Machines II course for a final classwide FIRST-style competition.
+          
+          Designed partially in SOLIDWORKS and custom-built using both storebought and self-cut parts. Programmed in C on an Arduino to allow for both manual controller-based and autonomous sensor-based navigation.`}
+          images={[
+            autonomousArduinoRobotImg1,
+            autonomousArduinoRobotImg2,
+            autonomousArduinoRobotImg3,
+          ]}
           links={[]}
           tags={["C", "Arduino", "SOLIDWORKS"]}
         />
         <ProjectCard
           h1="PID Line Follower"
           h2="Robotics project."
-          p={`Done as part of MIT's Introduction to Autonomous Machines course.`}
+          p={`Done as part of MIT's 16.632A Introduction to Autonomous Machines course for a final classwide competition.
+          
+          Coding in C for use on a pre-built Arduino robot, I wrote and tuned a PID controller designed to follow a complex path.
+          
+          Won 3rd place in the final competition.`}
           images={[PIDLineFollowerImg1]}
           links={[]}
           tags={["C", "Arduino"]}
